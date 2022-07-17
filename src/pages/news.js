f75e230a1b8b50a648a8d254b7328d782f3a024d
tomoya <sunda0707@icloud.com>
2022-07-17 13:06:35 +0900

    (#3) News 一覧を作成する

--- src/pages/news.js
+++ src/pages/news.js
@@ -16,12 +16,54 @@ const NewsTitle = () => {
   );
 };
 
+const NewsList = ({ news }) => {
+  return (
+    <ul className="news_list">
+      {news.map((news) => (
+        <li key={news.id} className="news_list__item">
+          <NewsItem {...news} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const NewsItem = ({ title, publishedAt, content }) => {
+  return (
+    <div className="news-item">
+      <div className="news-item__info">
+        <span className="news-item__published-at">{publishedAt}</span>
+      </div>
+      <h2 className="news-item__title">{title}</h2>
+      <article className="news-item__article">{content}</article>
+    </div>
+  );
+};
+
 const NewsPage = () => {
+  const news = [
+    {
+      id: 1,
+      title: "hoge",
+      content: "fuga",
+      publishedAt: "2022-01-01",
+    },
+    {
+      id: 2,
+      title: "hoge",
+      content: "fuga",
+      publishedAt: "2022-01-01",
+    },
+  ];
+
   return (
     <Layout>
       <div className="news">
         <div className="news__title">
           <NewsTitle />
+        </div>
+        <div className="news__list">
+          <NewsList news={news} />
         </div>
       </div>
     </Layout>
