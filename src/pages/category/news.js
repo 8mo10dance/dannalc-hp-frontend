5213b804588452981c41340987d1ebbb41a24cda
tomoya <sunda0707@icloud.com>
2022-07-18 17:39:55 +0900

    (#3) サイドメニューを実装する

--- src/pages/category/news.js
+++ src/pages/category/news.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 import Layout from "../../components/layout";
 import Title from "../../images/news/title.png";
 import Bg from "../../images/common/title-bg.png";
@@ -40,6 +41,73 @@ const NewsItem = ({ title, publishedAt, content }) => {
   );
 };
 
+const NewsSide = ({ recentlyNews, archives }) => {
+  return (
+    <div className="news_side">
+      <div className="news_side__card">
+        <h3 className="news_side__title">最近の投稿</h3>
+        <div className="news_side__body">
+          <RecentlyNewsList news={recentlyNews} />
+        </div>
+      </div>
+      <div className="news_side__card">
+        <h3 className="news_side__title">カテゴリー</h3>
+        <div className="news_side__body">
+          <NewsCategoryList />
+        </div>
+      </div>
+      <div className="news_side__card">
+        <h3 className="news_side__title">アーカイブ</h3>
+        <div className="news_side__body">
+          <NewsArchiveList archives={archives} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RecentlyNewsList = ({ news }) => {
+  return (
+    <ul className="recently-news-list">
+      {news.map((n) => (
+        <li key={n.id} className="recently-news-list__item">
+          <Link to="#">
+            <p className="recently-news-list__item-title">{n.title}</p>
+            <p className="recently-news-list__item-published-at">
+              {n.publishedAt}
+            </p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const NewsCategoryList = () => {
+  return (
+    <div className="news-category-list">
+      <Link to="/category/event/" className="news-category-list__item">
+        イベント
+      </Link>
+      <Link to="/category/news/" className="news-category-list__item">
+        お知らせ
+      </Link>
+    </div>
+  );
+};
+
+const NewsArchiveList = ({ archives }) => {
+  return (
+    <ul className="news-archive-list">
+      {archives.map((a) => (
+        <li key={`${a.year}-${a.month}`} className="news-archive-list__item">
+          <Link to={`/${a.year}/${a.month}/`}>{`${a.year}年${a.month}月`}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 const NewsPage = () => {
   const news = [
     {
@@ -56,6 +124,17 @@ const NewsPage = () => {
     },
   ];
 
+  const archives = [
+    {
+      year: 2022,
+      month: 7,
+    },
+    {
+      year: 2022,
+      month: 7,
+    },
+  ];
+
   return (
     <Layout>
       <div className="news">
@@ -64,6 +143,9 @@ const NewsPage = () => {
         </div>
         <div className="news__list">
           <NewsList news={news} />
+        </div>
+        <div className="news__side">
+          <NewsSide recentlyNews={news} archives={archives} />
         </div>
       </div>
     </Layout>
